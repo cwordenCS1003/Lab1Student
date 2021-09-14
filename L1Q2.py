ece5767d86a6099ad79d41a63c86d93ac401682c
cwordenCS1003 <90486666+cwordenCS1003@users.noreply.github.com>
2021-09-14 15:22:25 -0300

    Update L1Q2.py

--- L1Q2.py
+++ L1Q2.py
@@ -1,5 +1,12 @@
-# Your Student Name, Your Student Number
-# Your Partner's Name, Your Partner's Number
-# Your Other Partner's Name, Your Other Partner's Number (delete if no other partner)
+# Chayse Worden, 3717308
+# Aidan Jardine, 3717361
 
 # Write your code for Question 2 below
+
+print("Volume of the pyramid:", (6*8*3)/3, " units  cubed")
+
+print("Volume of the cube:", 6**3, "units cubed")
+
+print("Volume of the cylinder:", 3.14*(3**2)*2, " units cubed")
+
+print("Circumference of the circle:", 2*3.14*90.1346, " units long")
