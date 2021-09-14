6dc6625ef3db19c184373de2bd1300c7ba930e54
cwordenCS1003 <90486666+cwordenCS1003@users.noreply.github.com>
2021-09-14 15:23:46 -0300

    Update L1Q3.py

--- L1Q3.py
+++ L1Q3.py
@@ -1,5 +1,16 @@
-# Your Student Name, Your Student Number
-# Your Partner's Name, Your Partner's Number
-# Your Other Partner's Name, Your Other Partner's Number (delete if no other partner)
+# Chayse Worden, 3717308
+# Aidan Jardine, 3717361
 
 # Write your code for Question 3 below
+
+print("a)Bills:", 20.25//10)
+print("a)Quarters:", 2025 % 4)
+
+print("b)Bills:", 31.50//10)
+print("b)Quarters", 3150 % 8)
+
+print("c)Bills:", 125.75//10)
+print("c)Quarters:", 12575 % 24)
+
+print("d)Bills:", 800//10)
+print("d)Quarters:", 800 % 4)
